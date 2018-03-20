fe268ed986d31d67b74129a9f25aa1752b892bb7
Khyathi Raghavi Chandu <kchandu@compute-0-17.local>
2018-03-20 11:56:42 +0000

    prediction for a new sentence

--- charcnn/train.py
+++ charcnn/train.py
@@ -89,6 +89,7 @@ def eval(data_iter, model, args):
 
 
 def predict(text, model, text_field, label_feild, cuda_flag):
+    text = "this is my sample text for predict"
     assert isinstance(text, str)
     model.eval()
     # text = text_field.tokenize(text)
