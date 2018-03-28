a8482595ac6012cada6053231a2c4a833e82fff1
sumeet-iitg <sumeet.iitg@gmail.com>
2018-03-28 19:29:12 -0400

    removing dead code
    Scale is not called anywhere.

--- phase2/optimization/embed_regularize.py
+++ phase2/optimization/embed_regularize.py
@@ -3,15 +3,15 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-def embed_regularize(embed, words, dropout=0.1, scale=None):
+def embed_regularize(embed, words, dropout=0.1): #, scale=None):
   if dropout:
     mask = embed.weight.data.new().resize_((embed.weight.size(0), 1)).bernoulli_(1 - dropout).expand_as(embed.weight) / (1 - dropout)
     mask = Variable(mask)
     masked_embed_weight = mask * embed.weight
   else:
     masked_embed_weight = embed.weight
-  if scale:
-    masked_embed_weight = scale.expand_as(masked_embed_weight) * masked_embed_weight
+#   if scale:
+#     masked_embed_weight = scale.expand_as(masked_embed_weight) * masked_embed_weight
 
   padding_idx = embed.padding_idx
   if padding_idx is None:
