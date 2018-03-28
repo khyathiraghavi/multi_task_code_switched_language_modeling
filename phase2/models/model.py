772bfc7f207bdf01330886648917977716e72974
Khyathi Raghavi Chandu <kchandu@compute-0-17.local>
2018-03-28 14:44:13 -0400

    emptying

--- phase2/models/model.py
+++ phase2/models/model.py
@@ -9,61 +9,64 @@ from optimization.weight_drop import weight_drop
 
 class RNNModel(nn.Module):
 
-	def __init__(self, num_token):
-		super(RNNModel, self).__init__()
-		self.lockdrop = locked_dropout()
-		self.encoder = nn.Embedding(num_token, WORD_EMBEDDING_SIZE)
-		self.rnns = [torch.nn.LSTM(WORD_EMBEDDING_SIZE if l == 0 else NUMBER_HIDDEN, NUMBER_HIDDEN if l != NUMBER_LAYERS - 1 else WORD_EMBEDDING_SIZE, 1, dropout=DROPOUT_HID) for l in range(NUMBER_LAYERS)]
-		print(self.rnns)
-		if WEIGHT_DROP:
-			self.rnns = [weight_drop(rnn, ['weight_hh_l0'], dropout=WEIGHT_DROP) for rnn in self.rnns]
-		self.rnns = torch.nn.ModuleList(self.rnns)
-		self.decoder = nn.Linear(NUMBER_HIDDEN, num_token)
+    def __init__(self, num_token):
+        super(RNNModel, self).__init__()
+        self.lockdrop = locked_dropout()
+        self.encoder = nn.Embedding(num_token, WORD_EMBEDDING_SIZE)
+        self.rnns = []
+        self.rnns.append(getattr(nn, MODEL_TYPE)(WORD_EMBEDDING_SIZE, NUMBER_HIDDEN,1, dropout=DROPOUT_HID))
+        self.rnns.append(getattr(nn, MODEL_TYPE)(NUMBER_HIDDEN, NUMBER_HIDDEN, 1, dropout=DROPOUT_HID))
+        self.rnns.append(getattr(nn, MODEL_TYPE)(NUMBER_HIDDEN, WORD_EMBEDDING_SIZE, 1, dropout=DROPOUT_HID))
+        if WEIGHT_DROP:
+            self.rnns = [weight_drop(rnn, ['weight_hh_l0'], dropout=WEIGHT_DROP) for rnn in self.rnns]
+        self.rnns = torch.nn.ModuleList(self.rnns)
+        self.decoder = nn.Linear(NUMBER_HIDDEN, num_token)
 
-		if TIED:
-			self.decoder.weight = self.encoder.weight
+        if TIED:
+            self.decoder.weight = self.encoder.weight
 
-		
-		self.encoder.weight.data.uniform_(-WEIGHT_INIT, WEIGHT_INIT)
-		self.decoder.bias.data.fill_(0)
-		self.decoder.weight.data.uniform_(-WEIGHT_INIT, WEIGHT_INIT)
+        
+        self.encoder.weight.data.uniform_(-WEIGHT_INIT, WEIGHT_INIT)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-WEIGHT_INIT, WEIGHT_INIT)
 
 
-	def forward(self, input, hidden, return_h=False):
-		emb = embed_regularize(self.encoder, input, dropout=DROPOUT_EMB if self.training else 0)
+    def forward(self, input, hidden, return_h=False):
+        embedding = embed_regularize(self.encoder, input, dropout=DROPOUT_EMB if self.training else 0)
 
-		emb = self.lockdrop(emb, DROPOUT_INP)
+        dropped_embedding = self.lockdrop(embedding, DROPOUT_INP)
 
-		raw_output = emb
-		new_hidden = []
+        hid_all = []
 
-		raw_outputs = []
-		outputs = []
-		for layer, rnn_unit in enumerate(self.rnns):
-			raw_output, new_h = rnn_unit(raw_output, hidden[layer])
-			new_hidden.append(new_h)
-			raw_outputs.append(raw_output)
-			if layer <= NUMBER_LAYERS -2:
-				raw_output = self.lockdrop(raw_output, DROPOUT_HID)
-				outputs.append(raw_output)
-		hidden = new_hidden
+        rnns_before_drop = []
+        rnns_after_drop = []
 
-		output = self.lockdrop(raw_output, DROPOUT)
-		outputs.append(output)
-	
-		d0 = output.size(0)
-		d1 = output.size(1)
-		d2 = output.size(2)
-	
-		output = output.view(d0*d1, d2)
-		decoder_output = self.decoder(output)
-		result = decoder_output.view(d0,d1, decoder_output.size(1))
-		#if return_h:
-		return result, hidden, raw_outputs, outputs
-		#return result, hidden
+        for layer, rnn_unit in enumerate(self.rnns):
+            if layer == 0:
+            	rnn_out, rnn_hid = rnn_unit(dropped_embedding, hidden[layer])
+            else:
+            	rnn_out, rnn_hid = rnn_unit(rnn_out, hidden[layer])
+            hid_all.append(rnn_hid)
+            
+            rnns_before_drop.append(rnn_out)
+            if layer <= NUMBER_LAYERS -2:
+                rnn_out = self.lockdrop(rnn_out, DROPOUT_HID)
+                rnns_after_drop.append(rnn_out)
 
-	def init_hidden(self, bsz):
-		weight = next(self.parameters()).data
-		return [(Variable(weight.new(1, bsz, NUMBER_HIDDEN if l != NUMBER_LAYERS - 1 else WORD_EMBEDDING_SIZE).zero_()),
-				Variable(weight.new(1, bsz, NUMBER_HIDDEN if l != NUMBER_LAYERS - 1 else WORD_EMBEDDING_SIZE).zero_()))
-				for l in range(NUMBER_LAYERS)]
+        final_out = self.lockdrop(rnn_out, DROPOUT)
+        rnns_after_drop.append(final_out)
+    
+        d0 = final_out.size(0)
+        d1 = final_out.size(1)
+        d2 = final_out.size(2)
+    
+        final_out = final_out.view(d0*d1, d2)
+        decoder_output = self.decoder(final_out)
+        result = decoder_output.view(d0,d1, decoder_output.size(1))
+        return result, hid_all , rnns_before_drop, rnns_after_drop
+
+    def init_hidden(self, bsz):
+        weight = next(self.parameters()).data
+        return [(Variable(weight.new(1, bsz, NUMBER_HIDDEN if l != NUMBER_LAYERS - 1 else WORD_EMBEDDING_SIZE).zero_()),
+                Variable(weight.new(1, bsz, NUMBER_HIDDEN if l != NUMBER_LAYERS - 1 else WORD_EMBEDDING_SIZE).zero_()))
+                for l in range(NUMBER_LAYERS)]
